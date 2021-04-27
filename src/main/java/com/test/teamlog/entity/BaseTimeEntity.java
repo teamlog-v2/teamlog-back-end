1ca04a32ffcb0288e1b947e41eb09e12fe33feff
hamchu <64580070+hamchu@users.noreply.github.com>
2021-04-28 00:07:18 +0900

    [추가] BaseTimeEntity

---
+++ src/main/java/com/test/teamlog/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.test.teamlog.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "create_time")
+    private LocalDateTime createTime;
+
+    @LastModifiedDate
+    @Column(name = "update_time")
+    private LocalDateTime updateTime;
+}
