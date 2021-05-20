52a471df94553ab0fdef24b01fe8669e882c6c3d
hamchu <64580070+hamchu@users.noreply.github.com>
2021-05-20 20:50:38 +0900

    [수정] 불필요 코드 삭제

--- src/main/java/com/test/teamlog/service/PostService.java
+++ src/main/java/com/test/teamlog/service/PostService.java
@@ -20,7 +20,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
