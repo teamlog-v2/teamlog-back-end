6f2cda0dfc9f2765a0a28753e74ac72f2ad298a6
JIN <jduck1024@naver.com>
2023-08-27 16:02:15 +0900

    PROJECTMEMBER-1 Refactor: 프로젝트 멤버 클래스 정리

---
+++ src/main/java/com/test/teamlog/domain/projectmember/controller/ProjectMemberApiController.java
@@ -0,0 +1,69 @@
+package com.test.teamlog.domain.projectmember.controller;
+
+import com.test.teamlog.domain.account.dto.UserRequest;
+import com.test.teamlog.domain.projectmember.dto.ProjectMemberReadResponse;
+import com.test.teamlog.domain.projectmember.dto.ProjectMemberReadResult;
+import com.test.teamlog.global.security.UserAdapter;
+import com.test.teamlog.payload.ApiResponse;
+import com.test.teamlog.domain.projectmember.service.ProjectMemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Tag(name = "프로젝트 멤버 관리")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class ProjectMemberApiController {
+    private final ProjectMemberService projectMemberService;
+
+    @Operation(summary = "프로젝트 초대 수락")
+    @PostMapping("/projects/{projectId}/members")
+    public ResponseEntity<ApiResponse> create(@PathVariable("projectId") Long projectId,
+                                              @Parameter(hidden = true) @AuthenticationPrincipal UserAdapter currentUser) {
+        ApiResponse apiResponse = projectMemberService.create(projectId, currentUser.getUser());
+        return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "프로젝트 멤버 삭제")
+    @DeleteMapping("/projects/{projectId}/members")
+    public ResponseEntity<ApiResponse> delete(@PathVariable("projectId") long projectId,
+                                              @RequestParam(value = "userId", required = false) String userId,
+                                              @Parameter(hidden = true) @AuthenticationPrincipal UserAdapter currentUser) {
+        ApiResponse apiResponse;
+        // userId 없으면 탈퇴 있으면 추방
+        if (userId == null) {
+            apiResponse = projectMemberService.leaveProject(projectId, currentUser.getUser());
+        } else {
+            apiResponse = projectMemberService.expelMember(projectId, userId, currentUser.getUser());
+        }
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @Operation(summary = "프로젝트 멤버 조회")
+    @GetMapping("/projects/{projectId}/members")
+    public ResponseEntity<List<ProjectMemberReadResponse>> readAll(@PathVariable("projectId") Long projectId) {
+        final List<ProjectMemberReadResult> resultList = projectMemberService.readAll(projectId);
+        final List<ProjectMemberReadResponse> responseList = resultList.stream().map(ProjectMemberReadResponse::of).collect(Collectors.toList());
+
+        return new ResponseEntity<>(responseList, HttpStatus.OK);
+    }
+
+    // FIXME: 개선 포인트. 해당 API는 삭제 예정이며 우선 dto는 ProjectMemberRead- 로 사용 (형태가 동일하다)
+    @Deprecated
+    @Operation(summary = "프로젝트 멤버가 아닌 유저 조회")
+    @GetMapping("/projects/{projectId}/not-members")
+    public ResponseEntity<List<UserRequest.UserSimpleInfo>> readAllNotInProjectMember(@PathVariable("projectId") Long projectId) {
+        List<UserRequest.UserSimpleInfo> response = projectMemberService.readAllNotInProjectMember(projectId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/test/teamlog/domain/projectmember/service/ProjectMemberService.java
@@ -0,0 +1,104 @@
+package com.test.teamlog.domain.projectmember.service;
+
+import com.test.teamlog.domain.account.dto.UserRequest;
+import com.test.teamlog.domain.account.model.User;
+import com.test.teamlog.domain.account.repository.AccountRepository;
+import com.test.teamlog.domain.post.repository.PostRepository;
+import com.test.teamlog.domain.project.service.ProjectService;
+import com.test.teamlog.domain.projectmember.dto.ProjectMemberReadResult;
+import com.test.teamlog.entity.Project;
+import com.test.teamlog.entity.ProjectJoin;
+import com.test.teamlog.entity.ProjectMember;
+import com.test.teamlog.exception.BadRequestException;
+import com.test.teamlog.exception.ResourceForbiddenException;
+import com.test.teamlog.exception.ResourceNotFoundException;
+import com.test.teamlog.payload.ApiResponse;
+import com.test.teamlog.repository.ProjectJoinRepository;
+import com.test.teamlog.repository.ProjectMemberRepository;
+import com.test.teamlog.repository.ProjectRepository;
+import com.test.teamlog.service.FileStorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ProjectMemberService {
+    private final AccountRepository accountRepository;
+    private final ProjectRepository projectRepository;
+    private final PostRepository postRepository;
+    private final ProjectMemberRepository projectMemberRepository;
+    private final ProjectJoinRepository projectJoinRepository;
+    private final ProjectService projectService;
+    private final FileStorageService fileStorageService;
+
+    // 프로젝트 멤버 추가
+    @Transactional
+    public ApiResponse create(Long projectId, User currentUser) {
+        final Project project = projectService.findOne(projectId);
+        ProjectJoin projectJoin = projectJoinRepository.findByProjectAndUser(project, currentUser)
+                .orElseThrow(() -> new ResourceNotFoundException("ProjectInvitation", "ID", currentUser.getIdentification()));
+
+        // TODO: project_join과 함께 다시 생각해보기
+        if (!projectJoin.getIsInvited() || projectJoin.getIsAccepted()) throw new BadRequestException("잘못된 요청입니다.");
+
+        projectMemberRepository.save(ProjectMember.create(projectJoin.getProject(), projectJoin.getUser()));
+        return new ApiResponse(Boolean.TRUE, "프로젝트 멤버 가입 됨");
+    }
+
+    // 프로젝트 나가기
+    @Transactional
+    public ApiResponse leaveProject(Long projectId, User currentUser) {
+        final Project project = projectService.findOne(projectId);
+
+        if(project.getMaster().getIdentification().equals(currentUser.getIdentification())) {
+            throw new ResourceForbiddenException("마스터는 탈퇴할 수 없습니다.\n위임하고 다시 시도하세요.");
+        }
+
+        ProjectMember projectMember = projectMemberRepository.findByProjectAndUser(project, currentUser)
+                .orElseThrow(() -> new ResourceNotFoundException("ProjectMemeber", "UserId", currentUser.getIdentification()));
+        projectMemberRepository.delete(projectMember);
+
+        return new ApiResponse(Boolean.TRUE, "프로젝트 탈퇴 완료");
+    }
+
+    // 마스터 - 프로젝트 멤버 삭제
+    @Transactional
+    public ApiResponse expelMember(Long projectId, String userId, User currentUser) {
+        final Project project = projectService.findOne(projectId);
+        User user = accountRepository.findByIdentification(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+        projectService.validateMasterUser(project, currentUser);
+
+        ProjectMember projectMember = projectMemberRepository.findByProjectAndUser(project, user)
+                .orElseThrow(() -> new ResourceNotFoundException("ProjectMemeber", "UserId", userId));
+
+        projectMemberRepository.delete(projectMember);
+        return new ApiResponse(Boolean.TRUE, "멤버 삭제 완료");
+    }
+
+
+    // 프로젝트 멤버 조회
+    public List<ProjectMemberReadResult> readAll(Long projectId) {
+        final Project project = projectService.findOne(projectId);
+        List<ProjectMember> projectMemberList = projectMemberRepository.findByProject(project);
+
+        return projectMemberList.stream().map(ProjectMemberReadResult::of).toList();
+    }
+
+
+    // 프로젝트 멤버 아닌 유저 리스트
+    @Deprecated
+    public List<UserRequest.UserSimpleInfo> readAllNotInProjectMember(Long projectId) {
+        List<User> userList = accountRepository.getUsersNotInProjectMember(projectId);
+        List<UserRequest.UserSimpleInfo> response = new ArrayList<>();
+        for (User user : userList) {
+            response.add(new UserRequest.UserSimpleInfo(user));
+        }
+        return response;
+    }
+}
