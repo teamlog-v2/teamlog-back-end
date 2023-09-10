f619432b7d9f0f11b8425ce9811163d4b7a0e126
JIN <jduck1024@naver.com>
2023-09-10 20:28:43 +0900

    REFACTOR-4 Refact: post, postUpdateHistory 간 관계 정리

--- src/main/java/com/test/teamlog/domain/post/repository/PostRepositoryImpl.java
+++ src/main/java/com/test/teamlog/domain/post/repository/PostRepositoryImpl.java
@@ -2,11 +2,15 @@ package com.test.teamlog.domain.post.repository;
 
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.test.teamlog.domain.postupdatehistory.entity.PostUpdateHistory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import static com.test.teamlog.domain.post.entity.QPost.post;
 import static com.test.teamlog.domain.postlike.entity.QPostLike.postLike;
+import static com.test.teamlog.domain.postupdatehistory.entity.QPostUpdateHistory.postUpdateHistory;
 
 @Repository
 @RequiredArgsConstructor
@@ -24,6 +28,14 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
                         postLike.user.idx.eq(userIdx)
                 )
                 .fetchFirst() != null;
+    }
+
+    public List<PostUpdateHistory> findAllPostUpdateHistoryByPostId(Long postId) {
+        return jpaQueryFactory
+                .selectFrom(postUpdateHistory)
+                .where(postUpdateHistory.post.id.eq(postId))
+                .orderBy(postUpdateHistory.id.asc())
+                .fetch();
     }
 
 }
