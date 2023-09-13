5c785c5a9e3e39452c03fbdcb1a3a136deefe5e5
JIN <jduck1024@naver.com>
2023-09-14 01:17:12 +0900

    REFACTOR-4 Refact: Comment service만 사용하도록 수

---
+++ src/main/java/com/test/teamlog/domain/comment/repository/CommentRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.test.teamlog.domain.comment.repository;
+
+import com.test.teamlog.domain.comment.entity.Comment;
+import com.test.teamlog.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+
+public interface CommentRepositoryCustom {
+    Page<Comment> findParentCommentListByPost(@Param("post") Post post, Pageable pageable);
+
+    Page<Comment> getChildCommentsByParentComment(@Param("comment") Comment comment, Pageable pageable);
+}
