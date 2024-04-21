68d5577c50a65b382d2660077679c6e337122972
윤진 <jduck1024@naver.com>
2024-04-21 23:44:12 +0900

    :wrench: SETTING-12 jwt 관련 설정 추가

--- src/main/java/com/test/teamlog/global/config/SwaggerConfig.java
+++ src/main/java/com/test/teamlog/global/config/SwaggerConfig.java
@@ -3,35 +3,43 @@ package com.test.teamlog.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String SECURITY_SCHEME_KEY = "bearer-jwt";
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(getComponents())
+                .info(getApiInfo())
+                .addSecurityItem(getSecurityItem());
     }
 
-    private Info apiInfo() {
+    private static SecurityRequirement getSecurityItem() {
+        return new SecurityRequirement().addList(SECURITY_SCHEME_KEY);
+    }
+
+    private Components getComponents() {
+        return new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_KEY,
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .in(SecurityScheme.In.HEADER)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
+    }
+
+    private Info getApiInfo() {
         return new Info()
                 .title("TeamLog API 명세")
                 .version("v0.0.1")
                 .description("TeamLog API 명세");
 
     }
-
-//    private OpenApiCustomiser buildSecurityOpenApi() {
-//        return OpenApi -> OpenApi.addSecurityItem(new SecurityRequirement().addList("jwt token"))
-//                .getComponents()
-//                .addSecuritySchemes("jwt token", new SecurityScheme()
-//                        .name("Authorization")
-//                        .type(SecurityScheme.Type.HTTP)
-//                        .in(SecurityScheme.In.HEADER)
-//                        .bearerFormat("JWT")
-//                        .scheme("Bearer"));
-//    }
 }
